3f2eb94b8b06e944bc333e880b23908a6f4b53f1
Liam O <spikything@gmail.com>
2025-10-26 14:43:17 +0000

    Add vendored `tslib`

---
+++ vendor/tslib/tslib.d.ts
@@ -0,0 +1,36 @@
+export declare function __extends(d: any, b: any): void;
+export declare const __assign: {
+  <T, U>(target: T, source: U): T & U;
+  <T, U, V>(target: T, source1: U, source2: V): T & U & V;
+  <T, U, V, W>(target: T, source1: U, source2: V, source3: W): T & U & V & W;
+  (target: any, ...sources: any[]): any;
+};
+export declare function __rest<T extends object, K extends PropertyKey>(source: T, exclude: readonly K[]): Omit<T, K>;
+export declare function __decorate(decorators: Function[], target: any, key?: string | symbol, desc?: any): any;
+export declare function __param(paramIndex: number, decorator: Function): Function;
+export declare function __metadata(metadataKey: any, metadataValue: any): Function | undefined;
+export declare function __createBinding(object: object, module: object, key: PropertyKey, alias?: PropertyKey): void;
+export declare function __setModuleDefault(target: object, value: any): void;
+export declare function __exportStar(module: object, exports: object): void;
+export declare function __values(o: any): IterableIterator<any>;
+export declare function __read<T>(o: Iterable<T>, n?: number): T[];
+export declare function __spread(...args: any[]): any[];
+export declare function __spreadArrays(...args: any[][]): any[];
+export declare function __spreadArray<T>(to: T[], from: T[], pack?: boolean): T[];
+export declare class __await<T> {
+  constructor(v: T);
+  v: T;
+}
+export declare function __asyncGenerator(thisArg: any, _arguments: any, generator: Function): AsyncGenerator<any, any, any>;
+export declare function __asyncDelegator(o: any): AsyncGenerator<any, any, any>;
+export declare function __asyncValues(o: any): AsyncIterableIterator<any>;
+export declare function __makeTemplateObject(cooked: TemplateStringsArray, raw: string[]): TemplateStringsArray;
+export declare function __importStar<T>(mod: T): T & { default: T };
+export declare function __importDefault<T>(mod: T): T & { default: T };
+export declare function __classPrivateFieldGet<T extends object, V>(receiver: T, state: any, kind: "f" | "a" | "m", accessor?: () => V): V;
+export declare function __classPrivateFieldSet<T extends object, V>(receiver: T, state: any, value: V, kind: "f" | "a" | "m", accessor?: (value: V) => void): V;
+export declare function __classPrivateFieldIn(state: any, receiver: object): boolean;
+export declare function __addDisposableResource<T>(env: { stack: any[]; error?: any; hasError?: boolean }, value: T, async: boolean): T;
+export declare function __disposeResources(env: { stack: any[]; error?: any; hasError?: boolean }): any;
+export declare function __awaiter<T>(thisArg: any, _arguments: any, P: PromiseConstructorLike, generator: Function): Promise<T>;
+export declare function __generator(thisArg: any, body: Function): Generator<any, any, any>;
